58f105724fa66251aac1cac221cb1043fa8f68ff
Ravi Desai <ravi@ravidesai.com>
2020-06-30 11:48:22 -0400

    Collect issueId and make call to JIRA
    The "make call" part doesn't work due to CORS.  We need to set up a
    server that can handle the Oauth2 3LO flow, that will then send a
    bearer-token to React.  After that, React should be able to handle all
    the JIRA interaction.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import './App.css';
 
 function ConfigForm(props) {
@@ -10,6 +11,10 @@ function ConfigForm(props) {
 	<input type='text' onChange={props.handleChange} />
 	</label>
 	<label>
+	JIRA username:
+	<input type='text' onChange={props.handleUsernameChange} />
+	</label>
+	<label>
 	JIRA API Key:
 	<input type='password' onChange={props.handleApiKeyChange} />
 	</label>
@@ -18,10 +23,75 @@ function ConfigForm(props) {
     )
 }
 
+function GoToIssue(props) {
+  return (
+      <div>
+      <form onSubmit={props.onSubmit} >
+      <label>
+      Issue ID: <input type='text' id='gotoissue' /></label>
+      <input type='submit' value='Go' />
+    </form>
+      </div>
+  )
+}
+
+class CurrentIssue extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { issue: {} }
+  }
+
+  fetchData = () => {
+    axios.get(`https://${this.props.subdomain}.atlassian.net/rest/api/latest/issue/${this.props.issueId}`, {
+      auth: {
+	username: this.props.username,
+	password: this.props.password
+      }
+    })
+      .then(response => {
+	console.log(`Got issue ${this.props.issueId}`)
+	let json = JSON.parse(response.data)
+	this.setState({issue: json})
+	console.log('z1', json)
+      })
+      .catch(error => {
+	console.log(error)
+      })
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.issueId === prevProps.issueId) {
+      return false
+    }
+
+    this.fetchData()
+  }
+
+  render() {
+    if (this.props.issueId === '') {
+      return(
+	  <div>
+	  Please enter an issue ID.
+	  </div>
+      )
+    }
+
+    return(
+	<div>
+	{ JSON.stringify(this.state.issue) }
+	</div>
+    )
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {subdomain: '', apiKey: ''}
+    this.state = {
+      issueId: '',
+      subdomain: '',
+      username: '',
+      apiKey: ''}
   }
 
   handleApiKeyChange = (e) => {
@@ -30,13 +100,31 @@ class App extends React.Component {
   handleSubdomainChange = (e) => {
     this.setState({subdomain: e.target.value})
   }
+  handleUsernameChange = (e) => {
+    this.setState({username: e.target.value})
+  }
+
+  handleGoToIssueSubmit = (e) => {
+    e.preventDefault()
+    let foo = document.getElementById('gotoissue').value
+    console.log('z0', foo)
+    this.setState({issueId: foo})
+    console.log('z1', this.state)
+  }
 
   render() {
     return (
 	<div className="App">
 	<ConfigForm
+      handleUsernameChange={this.handleUsernameChange}
       handleApiKeyChange={this.handleApiKeyChange}
       handleChange={this.handleSubdomainChange} />
+	<GoToIssue onSubmit={this.handleGoToIssueSubmit} />
+	<CurrentIssue
+      issueId={this.state.issueId}
+      subdomain={this.state.subdomain}
+      username={this.state.username}
+      password={this.state.password} />
 	</div>
     );
   }
